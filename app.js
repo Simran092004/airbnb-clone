b6dff8b059afa7bcb3c5f8e2f5f35f2d6056c8c9
Simran092004 <yadavsimran@359@gmail.com>
2026-07-05 00:23:54 +0530

    fix home route

--- app.js
+++ app.js
@@ -101,6 +101,10 @@ app.use((req,res,next)=>{
 //     res.send(registeredUser);
 // });
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 app.use('/listings', listingRouter);
 app.use('/listings/:id/reviews', reviewRouter);
 app.use('/users', userRouter);
